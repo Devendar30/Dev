226488ad2bbe7a18e48e42211ce6229181e9ef37
Devendar30 <cheruku.devendar@gmail.com>
2021-02-09 19:42:31 +0530

    Adding one more line

--- Print.sh
+++ Print.sh
@@ -5,3 +5,12 @@
 
 echo Hello World
 echo welcome to DevOps
+
+# print messages with different colors
+# By using ESC sequences in echo qwe can print colors
+# /n - new line
+##  Note: when we use esc seq, Input to echo command should be given in double quotes (single quotes can also be used,
+#  but preferred is double quotes) , Also esc seq works when you enable -e option to echo command.
+
+
+echo -e "Hello,\nwelcome to devops Training"
