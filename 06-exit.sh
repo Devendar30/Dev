cb5978d8e75797ee32187e58bd3b653dd7ca024c
Devendar30 <cheruku.devendar@gmail.com>
2021-02-11 19:41:43 +0530

    Adding one more line

--- 06-exit.sh
+++ 06-exit.sh
@@ -4,4 +4,15 @@ echo hello
 exit
 echo bye
 
-##
+##exit command stop stop the script if it is encountered, No more commands will execute
+## simple exit command returns 0 as the text status.
+# and gives 0 when we give $?
+
+#Output for the above syntax is -> hello and we have exit in second line.
+
+echo hello
+exit 19
+echo bye
+
+#Output for the above syntax is -> hello and it has exits because we have exit command in second line.
+# and gives 19 when we give $?
