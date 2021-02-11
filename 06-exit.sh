9646cacb0e75ac0fc072c08d204a8a73aab73329
Devendar30 <cheruku.devendar@gmail.com>
2021-02-11 19:47:00 +0530

    Adding one more line

--- 06-exit.sh
+++ 06-exit.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#echo hello
-#exit
-#echo bye
-
-##exit command stop stop the script if it is encountered, No more commands will execute
-## simple exit command returns 0 as the text status.
-# and gives 0 when we give $?
-
-#Output for the above syntax is -> hello and we have exit in second line.
-
 echo hello
 exit 19
 echo bye
