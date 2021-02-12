d24a8c6b0152750a69fc8c9f86386d50937bc223
Devendar30 <cheruku.devendar@gmail.com>
2021-02-12 10:30:06 +0530

    Adding one more line

---
+++ 07-functions.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Declare a function
+
+#way2
+sample ()
+ {
+  echo Hello from sample function
+ }
+
+#way2
+function sample2()
+ {
+  echo Hello from sample1 function
+ }
+
+sample
+sample1
