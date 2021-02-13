a817c9d5b99afc6c983b593105d8948c01e510ad
Devendar30 <cheruku.devendar@gmail.com>
2021-02-13 11:15:00 +0530

    Adding one more line

--- 08-if.sh
+++ 08-if.sh
@@ -4,8 +4,10 @@ if [ "USER" != root ]; then
   echo Hey, you are not a root user.
   fi
 
-  read -p 'enter your name:' name
+read -p 'enter your name:' name
 
 if [ -z "$name" ]; then
     echo Hey, you have not entered your name
+else
+    echo Hey $name, Welcome.
 fi
