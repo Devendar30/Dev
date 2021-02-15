67f4027720c31501ba5f8707376cce6f7547086d
Devendar30 <cheruku.devendar@gmail.com>
2021-02-15 15:13:31 +0530

    Adding one more line

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -7,5 +7,6 @@ if [ -z "${component}" ]; then
   exit 1
 fi
 
-aws ec2 run-instances --launch-template LaunchTemplateId=lt-007ed01f0ecd19fd9 --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=${component}}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-007ed01f0ecd19fd9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
+Value=${component}}]"
 
