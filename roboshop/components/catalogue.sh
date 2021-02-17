ab2f4cae702eb9bc19fe4dd5058cba2123380b03
Devendar30 <cheruku.devendar@gmail.com>
2021-02-17 12:00:16 +0530

    Adding one more line

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -9,8 +9,8 @@ yum install nodejs make gcc-c++ -y
 stat $?
 
 Print "Adding roboshop user" "useradd roboshop"
-##useradd roboshop
-##stat $?
+useradd roboshop
+stat $?
 
 Print "Download catalogue COMPONENT Code"
 curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/d62914b9-61e7-4147
@@ -18,6 +18,7 @@ curl -s -L -o /tmp/catalogue.zip "https://dev.azure.com/DevOps-Batches/f635c088-
 &resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 
 stat $?
+
 Print "Extract catalogue component code" "rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue &&
 unzip /tmp/catalogue.zip"
 
